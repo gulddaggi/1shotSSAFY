5e0b5874260ba746611d36c2040942f2e8994810
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-02-21 15:37:04 +0900

    simple rotation
    간단한 회전

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody playerRb;
+    Vector3 fireVector;
+
+    public GameObject inputDegreeText;
+
     void Start()
     {
-        Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
-        rigidbody.AddForce(new Vector3(300.0f, 0.0f));
+        fireVector = Vector3.right;
+        playerRb = gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Debug.DrawLine(gameObject.transform.position, fireVector, Color.red);
+    }
+
+    public void shot()
+    {
+        playerRb.AddForce(fireVector * 400.0f);
+    }
+
+    public void rotate()
+    {
+        Debug.Log(inputDegreeText.GetComponent<InputField>().text);
+        int degree = int.Parse(inputDegreeText.GetComponent<InputField>().text);
+
+        fireVector = Quaternion.AngleAxis(degree, Vector3.up).normalized * fireVector;
     }
 }
